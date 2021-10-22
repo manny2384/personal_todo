f0ac80b2c77fc9dea8293ebaee9bfa14feccfb83
manny2384 <gonzalezeh2015@gmail.com>
2021-10-22 09:25:12 -0700

    fixed init date selection

--- src/CreateItem.js
+++ src/CreateItem.js
@@ -1,9 +1,19 @@
 function CreateItem(props){
 
+    let today = new Date();
+    console.log(today.getDate(), today.getMonth(), today.getFullYear());
+    let day = today.getDate();
+    let month = today.getMonth()+1;
+    let year = today.getFullYear();
+
+    if(day < 10) day = '0' + day;
+    if(month < 10) month = '0' + month;
+    
+
     return <div className="New_item_style">
         <div className="New_item_labels"_>
             <label> Item Name: <input id="New_Item" /> </label>
-            <label> Due Date: <input type="date" min="2021-10-30" max="2030-10-30" id="Duedate" /> </label>
+            <label> Due Date: <input type="date" min={year + '-' + month + '-' + day} max="2030-10-30" id="Duedate" /> </label>
         </div>
         <div className="New_item_btns">
             <input type="submit" value="Add Item" />
